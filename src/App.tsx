e59767999e2e9c3d0dff7be4dfd18aee114061e2
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-02-19 21:17:10 +0000

    added  lenis

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,57 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Vite from './pages/Vite';
 import GlassSurface from './components/GlassSurface';
+import { ReactLenis } from 'lenis/react'
+import lord_marshy_logo from './assets/lord_marshy_logo.png'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-nav-wrapper">
-        <GlassSurface 
-          width="100%"
-          height={70}
-          borderRadius={30}
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.1,           // Smoothness (0–1, lower = smoother)
+          duration: 1.2,       // Scroll duration in seconds
+          smoothWheel: true,   // Smooth mouse wheel (default: true)
+          wheelMultiplier: 1, // Scroll speed      
+        }}
+      >
+        <div className="app-nav-wrapper">
+          <GlassSurface 
+            width="100%"
+            height={70}
+            borderRadius={30}
+            displace={0.5}
+            distortionScale={-180}
+            redOffset={0}
+            greenOffset={10}
+            blueOffset={20}
+            brightness={50}
+            opacity={0.93}
+            borderWidth={0.2}
+            backgroundOpacity={0.1}
+            saturation={0.9}
+            mixBlendMode="screen"
 
-          displace={0.5}
-          distortionScale={-180}
-          redOffset={0}
-          greenOffset={10}
-          blueOffset={20}
-          brightness={50}
-          opacity={0.93}
-          borderWidth={0.2}
+            className="app-nav-glass"
+          >
+            <nav className="app-nav">
+              <img src={lord_marshy_logo} alt="website logo" className="h-12 rotate-4"/>
+              <div className="flex items-center gap-12">
+                <NavLink to="" end>GHome</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+                <NavLink to="/vite">Vite</NavLink>
+              </div>
+            </nav>
+          </GlassSurface>
+        </div>
 
-          backgroundOpacity={0.1}
-
-          saturation={0.9}
-          mixBlendMode="screen"
-          className="app-nav-glass"
-          
-        
-        >
-          <nav className="app-nav">
-            <NavLink to="" end>GHome</NavLink>
-            <NavLink to="/contact" >Contact</NavLink>
-            <NavLink to="/vite">Vite</NavLink>
-          </nav>
-        </GlassSurface>
-      </div>
-
-      <Routes>
-        <Route path="/vite" element={<Vite/>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route path="/vite" element={<Vite/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </ReactLenis>
     </BrowserRouter>
   )
 }
