ae768ec7d19f61ccf8c5b34df3ab92ba3156286d
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-02-19 16:16:43 +0000

    Navbar working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Vite from './pages/Vite';
@@ -7,6 +7,11 @@ import Vite from './pages/Vite';
 function App() {
   return (
     <BrowserRouter>
+      <nav className='app-nav' >
+        <NavLink to="" end>Home</NavLink>
+        <NavLink to="/contact" end>Contact</NavLink>
+        <NavLink to="/vite" >Vite</NavLink>
+      </nav>
       <Routes>
         <Route path="/vite" element={<Vite/>} />
         <Route path="/" element={<Home />} />
