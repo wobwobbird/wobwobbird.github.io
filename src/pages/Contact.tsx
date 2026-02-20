1fc125caf873bdaaeed54fd7f7503cb4c8c77d53
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-02-20 20:36:55 +0000

    improving  contact buttons on the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,18 @@
 // import Balatro from "@/components/Balatro";
 import { GridScan } from "@/components/GridScan";
+import GlassIcons from "@/components/GlassIcons";
+import { FiLinkedin, FiGithub, FiFileText } from 'react-icons/fi';
+import { LiaLinkedin } from "react-icons/lia";
+
 
 const Contact = () => {
+    // update with your own icons and colors
+    const items = [
+        { icon: <LiaLinkedin />, color: 'blue', label: 'LinkedIn' },
+        { icon: <FiGithub />, color: 'purple', label: 'GitHub' },
+        { icon: <FiFileText />, color: 'purple', label: 'CV' },
+    ];
+
     return (
         <div className="fixed inset-0 w-full h-full">
             {/* <Balatro mouseInteraction={false} spinRotation={-1.0} spinSpeed={3.0} /> */}
@@ -10,24 +21,32 @@ const Contact = () => {
                     sensitivity={0.1}
                     lineThickness={1}
                     linesColor="rgba(44, 81, 36, 0.87);"
-                    gridScale={0.1}
+                    gridScale={0.03}
                     scanColor="rgba(3, 196, 51, 0.87);"
                     scanOpacity={0.4}
                     enablePost
                     bloomIntensity={0.6}
                     chromaticAberration={0.002}
-                    noiseIntensity={0.01}
+                    noiseIntensity={0.05} //0.01
+                    // scanOnClick={true}
+                    // scanDelay={6}
+                    scanDuration={6}
                 />
             </div>
             <div
                 className=" relative z-10 contact-page"
             
             >
-                <h2 className="google-sans-flex-bold-round text-6xl pb-1" >Get in</h2>
+                <h2 className="google-sans-flex-bold-round text-6xl -mb-5" >Get in</h2>
                 <h2 className="google-sans-flex-bold-round text-8xl" >Contact</h2>
-                <h2 className="google-sans-flex-skinny text-6xl pt-5" >I don't bite</h2>
+                <h2 className="google-sans-flex-skinny text-5xl pt-0" >I don't bite</h2>
                 <h4></h4>
                 <div>
+
+                    <div style={{ height: '600px', position: 'relative' }}>
+                        <GlassIcons items={items} className="custom-class" iconSize="2em" />
+                    </div>
+
                     <div></div>
                 </div>
             </div>
