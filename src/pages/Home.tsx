1d7ab9f8cce5b5ddc0cc0cc8a27ac60aed1f780c
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-02-23 14:52:23 +0000

    Refactor Home to use new typography

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,11 +30,9 @@ const Home = () => {
           >NOTE WEBSITE STILL UNDER CONSTRUCTION</h1>
           <h1 className="google-sans-flex-bold-round text-4xl pt-10"
           >🚧🚧🚧</h1> */}
-          <p className="google-sans-flex-bold-round text-4xl md:text-6xl pt-14"
-          >Guy - Junior Full-Stack Dev</p>
+          <h1 className="pt-14">Guy - Junior Full-Stack Dev</h1>
           {/* Body - intro*/}
-          <h2 className="google-sans-flex-skinny text-3xl pt-5 text-left"
-          >Introduction</h2>
+          <h2 className="pt-5 text-left">Introduction</h2>
           <div className="flex flex-col sm:flex-row md:p-5 gap-4 ">
             <div className="h-auto p-2 flex flex-col gap-4 pt-5 flex-1" >
               <p>I'm a Full-Stack Developer based in London with two years of intensive self-driven learning and nine months of commercial experience at a PropTech startup.</p>
@@ -66,14 +64,14 @@ const Home = () => {
           {/* Body - Projects */}
 
           <div className="flex flex-col gap-4 text-left">
-            <h2 className="google-sans-flex-skinny text-3xl pt-5 text-left">Projects</h2>
+            <h2 className="pt-5 text-left">Projects</h2>
 
             {/* FIRST */}
             <div
               className="relative w-full min-h-50 flex flex-col gap-4 p-5 md:m-3 rounded-[1.25em] bg-[hsla(0,0%,100%,0.15)] backdrop-blur-[0.75em] [-webkit-backdrop-filter:blur(0.75em)] [-moz-backdrop-filter:blur(0.75em)]"
               style={{ boxShadow: '0 0 0 0.1em hsla(0, 0%, 100%, 0.3) inset' }}
             >
-              <h3 className="google-sans-flex-default text-3xl">Mood Tracker</h3>
+              <h3>Mood Tracker</h3>
               <p>Mental Health Tracker CLI tool, record mood and review results. Uses Ink to create a full screen TUI, saves results in a SQLite database.</p>
 
               <div className="flex flex-row gap-5 py-5">
@@ -104,7 +102,7 @@ const Home = () => {
               className="relative w-full min-h-50 flex flex-col gap-4 rounded-[1.25em] bg-[hsla(0,0%,100%,0.15)] backdrop-blur-[0.75em] [-webkit-backdrop-filter:blur(0.75em)] [-moz-backdrop-filter:blur(0.75em)] p-5 md:m-3"
               style={{ boxShadow: '0 0 0 0.1em hsla(0, 0%, 100%, 0.3) inset' }}
             >
-              <h3 className="google-sans-flex-default text-3xl">Tap 'O' Matic</h3>
+              <h3>Tap 'O' Matic</h3>
               <p>A React Native TypeScript random number generator app</p>
               <div className="flex flex-row gap-5 py-5">
                 <button className="w-[200px]">
@@ -132,7 +130,7 @@ const Home = () => {
               className="relative w-full min-h-50 flex flex-col gap-4 rounded-[1.25em] bg-[hsla(0,0%,100%,0.15)] backdrop-blur-[0.75em] [-webkit-backdrop-filter:blur(0.75em)] [-moz-backdrop-filter:blur(0.75em)] p-5 md:m-3"
               style={{ boxShadow: '0 0 0 0.1em hsla(0, 0%, 100%, 0.3) inset' }}
             >
-              <h3 className="google-sans-flex-default text-3xl">Super Connect</h3>
+              <h3>Super Connect</h3>
               <p>A two-player turn-based Connect 4–style game built in Unity 6 (URP). Get four in a row: horizontally, vertically, or diagonally to win. Full game with start scene, player naming, scoring, winning screen, music control, 3D background and animations</p>
               <div className="flex flex-row gap-5 py-5">
                 <button className="w-[200px]">
@@ -156,14 +154,14 @@ const Home = () => {
           
           {/* Body - Commercial */}
           <div className="text-left flex flex-col gap-4 pt-4">
-            <h2 className="google-sans-flex-skinny text-3xl pt-5 text-left">Commercial Experiance</h2>
+            <h2 className="pt-5 text-left">Commercial Experiance</h2>
 
             {/* FIRST */}
             <div
               className="relative w-full min-h-50 flex flex-col gap-4 rounded-[1.25em] bg-[hsla(0,0%,100%,0.15)] backdrop-blur-[0.75em] [-webkit-backdrop-filter:blur(0.75em)] [-moz-backdrop-filter:blur(0.75em)] p-5 md:m-3"
               style={{ boxShadow: '0 0 0 0.1em hsla(0, 0%, 100%, 0.3) inset' }}
             >
-              <h3 className="google-sans-flex-default text-3xl">iRealty PropTech</h3>
+              <h3>iRealty PropTech</h3>
               <p>Worked as a Junior Front-End Developer at the PropTech startup Converted the customer facing CRM to tablet and mobile responsive, and built a customer support pannel available across the app Built property filtering, comparison tools, and multilingual support (Catalan/French) </p>
 
               <div className="flex flex-row gap-5 py-5">
@@ -189,8 +187,7 @@ const Home = () => {
           </div>
           {/* Body - Learning */}
           <div className="text-left pb-[150px]" >
-            <h2 className="google-sans-flex-skinny text-3xl pt-5 text-left"
-            >Learning</h2>
+            <h2 className="pt-5 text-left">Learning</h2>
             <p>I have been putting in the effort to learn about many different areas</p>
             <div className="flex flex-row gap-2">
               <p>So my learning certificates on my linkedIn:</p>
