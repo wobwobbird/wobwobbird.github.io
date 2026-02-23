936f0cb131424ef74e29776e6175a795ccab9055
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2026-02-23 13:13:23 +0000

    Created a page holder functional component and implemented it in Entry

---
+++ src/components/PageHolder.tsx
@@ -0,0 +1,25 @@
+interface PageGolderProps {
+    background?: React.ReactNode;
+    children: React.ReactNode;
+    className?: string;
+}
+
+
+const PageHolder = ({ background, children, className }: PageGolderProps) => {
+    return (
+        <div className="fixed inset-0 w-full h-full">
+            <div className="absolute inset-0" >
+                {background}
+            </div>
+            <div
+                className="relative z-10 flex items-center justify-center w-full h-full"
+            >
+                <div className={"w-full max-w-[800px] md:px-4 flex flex-col" + (className ? ` ${className}` : "")} >
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageHolder;
--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -1,9 +1,72 @@
 import LetterGlitch from "@/components/LetterGlitch";
 import GlassSurface from '../components/GlassSurface';
 import { Link } from "react-router-dom";
+import PageHolder from "@/components/PageHolder";
 
 
 const Entry = () => {
+
+    return (
+        <PageHolder 
+            background={
+                <LetterGlitch 
+                    glitchColors={['#2b4539', '#61dca3', '#61b3dc']}
+                    glitchSpeed={100}
+                    centerVignette={true}
+                    outerVignette={false}
+                    smooth={true}
+                    // characters="MARSH"
+                    characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$&*()-_+=/[]{};:<>.,0123456789"
+                />
+            } 
+            className="items-center"
+
+        >
+
+            <h2 className="google-sans-flex-bold-round text-4xl md:text-6xl md:-mb-5 drop-shadow-[0_0_6px_rgba(0,0,0,0.9)]"
+            >Welcome to</h2>
+            {/* <h2 className="google-sans-flex-bold-round text-6xl -mb-5 drop-shadow-[0_0_6px_rgba(0,0,0,0.9)]" >Welcome to</h2> */}
+            <h2 className="google-sans-flex-bold-round text-6xl md:text-8xl max-w-full text-center drop-shadow-[0_0_6px_rgba(0,0,0,0.9)]"
+            >Guy's Website</h2>
+            <h2 className="google-sans-flex-bold-round text-3xl md:text-4xl max-md:pt-4 drop-shadow-[0_0_6px_rgba(0,0,0,0.9)]"
+            >Junior Full-Stack Software Developer</h2>
+            <Link to="/home" className="block w-[90%] sm:w-[600px] mt-10">
+                <GlassSurface
+                    className="cursor-pointer w-full"
+                    width="100%"
+                    height={70}
+                    borderRadius={30}
+                    displace={0.01}
+                    distortionScale={0}
+                    redOffset={0}
+                    greenOffset={-50}
+                    blueOffset={20}
+                    brightness={50}
+                    opacity={0}
+                    borderWidth={0.2}
+                    backgroundOpacity={0.4}
+                    saturation={0.9}
+                    mixBlendMode="screen"
+                >
+                    <p
+                        className="google-sans-flex-contact text-5xl"
+                    >Enter</p>
+                </GlassSurface>
+            </Link>
+
+
+            <div>
+                <div className="h-auto relative" >
+                    {/* <GlassIcons items={items} className="custom-class" /> */}
+                </div>
+            </div>
+
+
+        </PageHolder>
+    )
+}
+
+const OLD = () => {
 
     return (
         <div className="fixed inset-0 w-full h-full">
